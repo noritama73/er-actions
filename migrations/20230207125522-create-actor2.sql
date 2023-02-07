f40742585b76f33d5e0bc8e3be372923dcabf609
noritama73 <takuma.shioiri.eiko.rbb@gmail.com>
2023-02-07 12:56:03 +0900

    fix

---
+++ migrations/20230207125522-create-actor2.sql
@@ -0,0 +1,11 @@
+CREATE TABLE actor2 (
+  actor_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(45) NOT NULL,
+  last_name VARCHAR(45) NOT NULL,
+  last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY  (actor_id),
+  KEY idx_actor_last_name (last_name)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- +migrate Up
+
+-- +migrate Down
